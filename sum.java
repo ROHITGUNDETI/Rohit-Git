76e77fd2f3f22d60ca245fc213d744ef63b628ed
rohit gundeti <rohitpgundeti@gmail.com>
2024-09-17 16:34:24 +0530

    added

--- sum.java
+++ sum.java
@@ -6,7 +6,8 @@ public class sum {
             public static void main(String[] args) {
                 int a =3;
                 int b =4;
+                int c = 3;
                 
-                System.out.println(a+b);
+                System.out.println(a+b+c);
             }
 }
